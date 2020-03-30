1cfbee18ec8b91d8a415c85086efd5a55131f23a
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-03-30 22:34:10 +0300

    Create script for updating time series

---
+++ src/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+update_data() {
+  git config --global user.email "maciv@hotmail.gr"
+  git config --global user.name "Isabelle Viktoria Maciohsek"
+  git checkout master
+  git pull
+  git commit -m "Update - $(date +%Y.%m.%d_%H:%M)" --allow-empty
+  git push --quiet "https://${GH_TOKEN}@github.com/Trinityyi/COVID-19-tools.git master" > /dev/null 2>&1
+}
+
+update_data
