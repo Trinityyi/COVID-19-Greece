c1fff02575226127724870a6e9cf29663408ac27
Isabelle Viktoria Maciohsek <maciv@hotmail.gr>
2020-03-30 23:31:40 +0300

    Update update.sh

--- src/update.sh
+++ src/update.sh
@@ -4,6 +4,7 @@ update_data() {
   git config --global user.name "Isabelle Viktoria Maciohsek"
   git checkout master
   git pull
+  git add .
   git commit -m "Update - $(date +%Y.%m.%d_%H:%M)" --allow-empty
   git push --quiet "https://${GH_TOKEN}@github.com/Trinityyi/COVID-19-tools.git" master > /dev/null 2>&1
 }
